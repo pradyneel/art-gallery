07115dee8b9962f0d57ba81546d5cf161c9774f5
RAC IT SOLUTIONS <racitsolutions@RACs-MacBook-Pro.local>
2023-01-25 15:46:45 +0530

    added whitenoise to serve static files

--- art/settings.py
+++ art/settings.py
@@ -48,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'art.urls'
@@ -122,3 +124,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
---
+++ staticfiles/home/updateQR.js
@@ -0,0 +1,25 @@
+function updateQR(image_id){
+
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Token bd2149fd3ad6748f72ebae26c7ceed035af67084");
+    myHeaders.append("content-type", "application/json")
+
+    let new_name = prompt("Enter new name for QR code");
+    
+
+    let raw = JSON.stringify({
+        "name": new_name,
+    });
+
+    var requestOptions = {
+    method: 'PUT',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+    };
+
+    fetch("https://api.beaconstac.com/api/2.0/qrcodes/" + image_id.toString() + "/", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+}
